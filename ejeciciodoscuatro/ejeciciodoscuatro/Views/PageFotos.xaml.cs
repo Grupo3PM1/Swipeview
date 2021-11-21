45ef82b5a2dd13aba61b7045cf8c843a6ab90966
Dans-sys <danielsolano712@gmail.com>
2021-11-20 22:43:51 -0600

    Swipeview

---
+++ ejeciciodoscuatro/ejeciciodoscuatro/Views/PageFotos.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ejeciciodoscuatro.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PageFotos : ContentPage
+    {
+        public PageFotos()
+        {
+            InitializeComponent();
+            cargarList();
+        }
+        public async void cargarList()
+        {
+            var fotoList = await App.SQLiteBD.GetFotoAsync();
+            if (fotoList != null)
+            {
+                lstFotos.ItemsSource = fotoList;
+            }
+        }
+    }
+}
